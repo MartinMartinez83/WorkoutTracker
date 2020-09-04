1705c1a51c7ed62f4fd3f3f89e3df8350d9b3f39
Martin Martinez <martinlmartinez1983@gmail.com>
2020-09-04 02:09:37 -0600

    mongodb

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 const app = express();
 
@@ -25,3 +26,5 @@ app.use(require("./routes/view.js"));
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+//mongodb+srv://MartinMartinez83:Vatoloco10!@workouttracker.djjw6.mongodb.net/workout?retryWrites=true&w=majority
